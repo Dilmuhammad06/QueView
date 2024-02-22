57f608742b1649d309f53a67b1938a34eb8e24e4
Dilmuhammad <145844965+Dilmuhammad06@users.noreply.github.com>
2024-02-22 16:56:20 +0500

    Delete myapp directory

--- myapp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from.models import Video
-
-class VideoForms(forms.ModelForm):
-    class Meta:
-        model = Video
-        fields = ['name','description','thumbnail','video','slug']
